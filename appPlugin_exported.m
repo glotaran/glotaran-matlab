044f1e3a8b57522f4699ae01879fb24bb65b57c9
Avratanu Biswas <a.biswas@vu.nl>
2019-03-25 16:32:48 +0100

    appPlugin_exported.m

--- appPlugin_exported.m
+++ appPlugin_exported.m
@@ -11,10 +11,15 @@ classdef appPlugin_exported < matlab.apps.AppBase
         PlotButton             matlab.ui.control.Button
         UIAxes                 matlab.ui.control.UIAxes
         UIAxes2                matlab.ui.control.UIAxes
+        SmoothDASSwitchLabel   matlab.ui.control.Label
+        SmoothDASSwitch        matlab.ui.control.Switch
+        CheckMaximaButton      matlab.ui.control.Button
         Sequential_Model       matlab.ui.container.Tab
         UIAxes3                matlab.ui.control.UIAxes
         UIAxes4                matlab.ui.control.UIAxes
         PlotButton_2           matlab.ui.control.Button
+        SmoothSASSwitchLabel   matlab.ui.control.Label
+        SmoothSASSwitch        matlab.ui.control.Switch
         Residual_Conc_profile  matlab.ui.container.Tab
         UIAxes5                matlab.ui.control.UIAxes
         UIAxes6                matlab.ui.control.UIAxes
@@ -24,8 +29,8 @@ classdef appPlugin_exported < matlab.apps.AppBase
     end
 
 
-   properties (Access = public)
-        das 
+   properties (Access = public) 
+        das         
         sas
         wavelength
         time        
@@ -34,10 +39,26 @@ classdef appPlugin_exported < matlab.apps.AppBase
         fitdata
         lsv
         rsv
-        residual  % Description
+        residual 
+        txt2 % call within other methods
     end
 
     
+
+    
+    methods (Access = public)
+        %not working
+        function locmax(app,wavelength,das)
+                        
+                [das_max,idx]= max(das);
+                wavelength_max = wavelength(idx,1);
+                txt = num2str(das_max,4);%Precision upto 4 digits 
+                txt2 = num2str(wavelength_max,3);
+                txt2
+%                 t=text(app.UIAxes2,app.wavelength_max,app.das_max,txt2,'Color','black','FontSize',10);
+            
+        end
+    end
 
 
     % Callbacks that handle component events
@@ -45,12 +66,12 @@ classdef appPlugin_exported < matlab.apps.AppBase
 
         % Button pushed function: FileButton
         function FileButtonPushed(app, event)
-            
+         % read the files and push the variables into properties   
            [filename,path] = uigetfile('*.nc');
            fullpathname = strcat(path,filename);
               
-             fullpathname = strcat(path,filename);
-             ncdisp (filename); % info regarding the file
+           fullpathname = strcat(path,filename);
+%              ncdisp (filename); % info regarding the file
              nc_header = ncinfo(filename);
              variable_names = {nc_header.Variables.Name};
              supported_variables = ~contains({nc_header.Variables.Datatype},'UNSUPPORTED');
@@ -95,13 +116,13 @@ classdef appPlugin_exported < matlab.apps.AppBase
         % Button pushed function: PlotButton
         function PlotButtonPushed(app, event)
 
-
+              normdas = [];
            %smoothening the spectras 
-           dastra = app.das';
+%            dastra = app.das';
           
-           dassm = smooth(dastra,'sgolay');
-           dasr= reshape(dassm,[size(dastra,1),size(dastra,2)]);
-           app.das= dasr';  %final das after smoothening 
+%            dassm = smooth(dastra,'sgolay');
+%            dasr= reshape(dassm,[size(dastra,1),size(dastra,2)]);
+%            app.das= dasr';  %final das after smoothening 
             for i=1:size(app.das,1)
                 das_max = max(abs(app.das(i,:)));
                 normdas(i,:) = app.das(i,:) / das_max;
@@ -112,11 +133,12 @@ classdef appPlugin_exported < matlab.apps.AppBase
 
         % Button pushed function: PlotButton_2
         function PlotButton_2Pushed(app, event)
-           
-        sastra = app.sas';
-        sassm = smooth(sastra,'sgolay');
-        sasr= reshape(sassm,[size(sastra,1),size(sastra,2)]);
-        app.sas= sasr'; %final das after smootheninh
+            normsas =[];
+            
+%         sastra = app.sas';
+%         sassm = smooth(sastra,'sgolay');
+%         sasr= reshape(sassm,[size(sastra,1),size(sastra,2)]);
+%         app.sas= sasr'; %final das after smootheninh
                 for i=1:size(app.sas,1)
             sas_max = max(app.sas(i,:));
             normsas(i,:) = app.sas(i,:) / sas_max;
@@ -140,6 +162,79 @@ classdef appPlugin_exported < matlab.apps.AppBase
             imagesc(app.UIAxes6,app.time,app.wavelength,app.residual);
             
  
+        end
+
+        % Value changed function: SmoothSASSwitch
+        function SmoothSASSwitchValueChanged(app, event)
+            value = app.SmoothSASSwitch.Value;
+           SmoothSwitchValue = app.SmoothSASSwitch.Value;
+            if ( strcmpi(SmoothSwitchValue,'on') )
+                %app.functionA;
+            sastra = app.sas';
+        sassm = smooth(sastra,'sgolay');
+        sasr= reshape(sassm,[size(sastra,1),size(sastra,2)]);
+        sassm= sasr'; %final das after smootheninh
+                for i=1:size(app.sas,1)
+            sas_max = max(sassm(i,:));
+            smnormsas(i,:) = sassm(i,:) / sas_max;
+              end
+            plot(app.UIAxes3,app.wavelength,sassm)
+            plot(app.UIAxes4,app.wavelength,smnormsas)
+            
+            else
+%                 app.functionB;
+                      for i=1:size(app.sas,1)
+            sas_max = max(app.sas(i,:));
+            normsas(i,:) = app.sas(i,:) / sas_max;
+        %     normsas(i,:)=locmax(spectra,normsas(i,:));
+                end
+            plot(app.UIAxes3,app.wavelength,app.sas)
+            plot(app.UIAxes4,app.wavelength,normsas) 
+            end
+            
+           
+        end
+
+        % Value changed function: SmoothDASSwitch
+        function SmoothDASSwitchValueChanged(app, event)
+            normdas =[];
+            dassm =[];
+            value = app.SmoothDASSwitch.Value;
+            SmoothDASSwitchValue = app.SmoothDASSwitch.Value;
+            if ( strcmpi(SmoothDASSwitchValue,'on') )
+                %app.functionA;
+            dastra = app.das';
+            dassm = smooth(dastra,'sgolay');
+            dasrsm = reshape(dassm,[size(dastra,1),size(dastra,2)]);
+            dassm = dasrsm';    %final das after smootheninh
+                for i=1:size(app.das,1)
+            smdas_max = max(abs(dassm(i,:)));
+            smnormdas(i,:) = dassm(i,:) / smdas_max;
+                end
+            plot(app.UIAxes,app.wavelength,dassm)
+            plot(app.UIAxes2,app.wavelength,smnormdas)
+            
+            else
+                
+                for i=1:size(app.das,1)
+            das_max = max(abs(app.das(i,:)));
+            normdas(i,:) = app.das(i,:) / das_max;
+        %     normsas(i,:)=locmax(spectra,normsas(i,:));
+                      end
+            plot(app.UIAxes,app.wavelength,app.das)
+            plot(app.UIAxes2,app.wavelength,normdas) 
+            end
+        end
+
+        % Button pushed function: CheckMaximaButton
+        function CheckMaximaButtonPushed(app, event)
+         locmax(app,app.wavelength,app.das)  
+          
+
+          text(app.UIAxes2,app.wavelength,app.das,txt2,'Color','black','FontSize',10);
+
+
+
         end
     end
 
@@ -211,23 +306,45 @@ classdef appPlugin_exported < matlab.apps.AppBase
             app.UIAxes2.YGrid = 'on';
             app.UIAxes2.Position = [141 1 300 185];
 
+            % Create SmoothDASSwitchLabel
+            app.SmoothDASSwitchLabel = uilabel(app.Parallel_Model);
+            app.SmoothDASSwitchLabel.HorizontalAlignment = 'center';
+            app.SmoothDASSwitchLabel.Position = [32 273 75 22];
+            app.SmoothDASSwitchLabel.Text = 'Smooth DAS';
+
+            % Create SmoothDASSwitch
+            app.SmoothDASSwitch = uiswitch(app.Parallel_Model, 'slider');
+            app.SmoothDASSwitch.ValueChangedFcn = createCallbackFcn(app, @SmoothDASSwitchValueChanged, true);
+            app.SmoothDASSwitch.Position = [46 310 45 20];
+
+            % Create CheckMaximaButton
+            app.CheckMaximaButton = uibutton(app.Parallel_Model, 'push');
+            app.CheckMaximaButton.ButtonPushedFcn = createCallbackFcn(app, @CheckMaximaButtonPushed, true);
+            app.CheckMaximaButton.Position = [492 349 100 22];
+            app.CheckMaximaButton.Text = {'Check Maxima'; ''};
+
             % Create Sequential_Model
             app.Sequential_Model = uitab(app.TabGroup);
             app.Sequential_Model.Title = 'Sequential_Model';
 
             % Create UIAxes3
             app.UIAxes3 = uiaxes(app.Sequential_Model);
-            title(app.UIAxes3, 'Title')
-            xlabel(app.UIAxes3, 'X')
-            ylabel(app.UIAxes3, 'Y')
+            title(app.UIAxes3, 'Species-associated spectra (SAS)')
+            xlabel(app.UIAxes3, 'Wavelength (nm)')
+            ylabel(app.UIAxes3, 'SAS')
+            app.UIAxes3.FontAngle = 'italic';
+            app.UIAxes3.FontWeight = 'bold';
             app.UIAxes3.Box = 'on';
             app.UIAxes3.Position = [161 203 300 185];
 
             % Create UIAxes4
             app.UIAxes4 = uiaxes(app.Sequential_Model);
-            title(app.UIAxes4, 'Title')
-            xlabel(app.UIAxes4, 'X')
-            ylabel(app.UIAxes4, 'Y')
+            title(app.UIAxes4, 'Species-associated spectra (norm)')
+            xlabel(app.UIAxes4, 'Wavelength (nm)')
+            ylabel(app.UIAxes4, 'SAS')
+            app.UIAxes4.FontAngle = 'italic';
+            app.UIAxes4.FontWeight = 'bold';
+            app.UIAxes4.YLim = [0 1.2];
             app.UIAxes4.Box = 'on';
             app.UIAxes4.Position = [161 1 300 185];
 
@@ -237,6 +354,17 @@ classdef appPlugin_exported < matlab.apps.AppBase
             app.PlotButton_2.Position = [10 366 100 22];
             app.PlotButton_2.Text = 'Plot';
 
+            % Create SmoothSASSwitchLabel
+            app.SmoothSASSwitchLabel = uilabel(app.Sequential_Model);
+            app.SmoothSASSwitchLabel.HorizontalAlignment = 'center';
+            app.SmoothSASSwitchLabel.Position = [23.5 287 74 22];
+            app.SmoothSASSwitchLabel.Text = {'Smooth SAS'; ''};
+
+            % Create SmoothSASSwitch
+            app.SmoothSASSwitch = uiswitch(app.Sequential_Model, 'slider');
+            app.SmoothSASSwitch.ValueChangedFcn = createCallbackFcn(app, @SmoothSASSwitchValueChanged, true);
+            app.SmoothSASSwitch.Position = [37 324 45 20];
+
             % Create Residual_Conc_profile
             app.Residual_Conc_profile = uitab(app.TabGroup);
             app.Residual_Conc_profile.Title = 'Res';
@@ -244,30 +372,30 @@ classdef appPlugin_exported < matlab.apps.AppBase
             % Create UIAxes5
             app.UIAxes5 = uiaxes(app.Residual_Conc_profile);
             title(app.UIAxes5, 'Concentration profile')
-            xlabel(app.UIAxes5, 'X')
-            ylabel(app.UIAxes5, 'Y')
+            xlabel(app.UIAxes5, 'Time (ns)')
+            ylabel(app.UIAxes5, 'Concentration')
             app.UIAxes5.Position = [9 216 300 185];
 
             % Create UIAxes6
             app.UIAxes6 = uiaxes(app.Residual_Conc_profile);
-            title(app.UIAxes6, 'Title')
-            xlabel(app.UIAxes6, 'X')
-            ylabel(app.UIAxes6, 'Y')
-            app.UIAxes6.Position = [308 216 300 185];
+            title(app.UIAxes6, {'Residuals'; ''})
+            xlabel(app.UIAxes6, 'Time (ns)')
+            ylabel(app.UIAxes6, 'Wavelength (nm)')
+            app.UIAxes6.Position = [310 216 300 185];
 
             % Create UIAxes7
             app.UIAxes7 = uiaxes(app.Residual_Conc_profile);
-            title(app.UIAxes7, 'Title')
-            xlabel(app.UIAxes7, 'X')
-            ylabel(app.UIAxes7, 'Y')
+            title(app.UIAxes7, {'Left singular vector'; ''})
+            xlabel(app.UIAxes7, 'Wavelength')
+            ylabel(app.UIAxes7, 'Intensity')
             app.UIAxes7.Position = [9 15 300 185];
 
             % Create UIAxes8
             app.UIAxes8 = uiaxes(app.Residual_Conc_profile);
-            title(app.UIAxes8, 'Title')
-            xlabel(app.UIAxes8, 'X')
-            ylabel(app.UIAxes8, 'Y')
-            app.UIAxes8.Position = [308 15 300 185];
+            title(app.UIAxes8, 'Right singular vector')
+            xlabel(app.UIAxes8, 'Time (ns)')
+            ylabel(app.UIAxes8, 'Intensity')
+            app.UIAxes8.Position = [310 15 300 185];
 
             % Create ResultsButton
             app.ResultsButton = uibutton(app.Residual_Conc_profile, 'push');
