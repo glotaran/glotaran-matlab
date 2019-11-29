1fa49a4568389b03f1409a8c9c0472e810e28d18
AvratanuBiswas <a.biswas@vu.nl>
2019-11-29 15:22:25 +0100

    ..

---
+++ data_check/get_ascii.m
@@ -0,0 +1,21 @@
+function [fileName,pp] = get_ascii(filename)
+persistent lastPath pathName
+% If this is the first time running the function this session,
+% Initialize lastPath to 0
+if isempty(lastPath) 
+    lastPath = 0;
+end
+% First time calling 'uigetfile', use the pwd
+if lastPath == 0
+    [fileName, pathName] = uigetfile('*.ascii');
+    pp = pathName;
+% All subsequent calls, use the path to the last selected file
+else
+    [fileName, pathName] = uigetfile(strcat(lastPath,'*.ascii'));
+    pp = pathName;
+end
+% Use the path to the last selected file
+% If 'uigetfile' is called, but no item is selected, 'lastPath' is not overwritten with 0
+if pathName ~= 0
+    lastPath = pathName;
+end
