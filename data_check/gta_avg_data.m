2f6b3fefe94a02046e81f985cf52648b70d5a552
AvratanuBiswas <a.biswas@vu.nl>
2020-02-11 14:00:35 +0000

    quick update

--- data_check/gta_avg_data.m
+++ data_check/gta_avg_data.m
@@ -2,8 +2,15 @@ function [specAvg,timeAvg] = gta_avg_data(dataf,wavelength,time)
 %% avergae of the data in spectral axis or time axis
 % mainly for power study
 %%
-timeAvg = sum(dataf);
-specAvg = sum(dataf,2);
-figure;plot(time,timeAvg)
-figure;plot(wavelength,specAvg)
+timeAvg = gta_norm(sum(dataf));
+specAvg = gta_norm(sum(dataf,2));
+figure;plot(time,timeAvg,'k','LineWidth',1.5); title('Avg along the all the wavelength - "Decay trace"');
+xlabel('Time (ps)'); ylabel('Normalized Intensity')
+ylim([0 1.1])
+
+figure;plot(wavelength,specAvg,'k','LineWidth',1.5); 
+title('Avg along the all the time points - Reconstructed SS')
+xlabel('Wavelength (nm)'); ylabel('Normalized Intensity')
+
+ylim([0 1.1])
 end
